c7abdb842654c41401950cf012cc7aabbc447846
QA-Cubix <qa.tusharchavan09@gmail.com>
2025-04-01 11:55:22 +0530

    Update3 Program.cs

--- JenkinsSampleProject/JenkinsSampleProject/Program.cs
+++ JenkinsSampleProject/JenkinsSampleProject/Program.cs
@@ -11,9 +11,12 @@ namespace JenkinsSampleProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+             Console.ReadLine();
+            Console.WriteLine("Tushar, World!");
+             Console.ReadLine();
+            Console.WriteLine("Chavan, World!");
+             Console.ReadLine();
+            Console.WriteLine("Bye, World!");
             Console.ReadLine();
         }
     }
